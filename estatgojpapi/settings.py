a211519e11fb295728c6ec5898707ad5f94a9369
osoken <osoken@osoekawalab.com>
2024-06-02 18:00:02 +0900

    feat(settings): read settings from .env

--- estatgojpapi/settings.py
+++ estatgojpapi/settings.py
@@ -8,5 +8,7 @@ class BaseSettings(PydanticBaseSettings):
 
 
 class AppSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
     app_id: str
     base_url: AnyHttpUrl = AnyHttpUrl("https://api.e-stat.go.jp/rest/3.0/app/json/")
