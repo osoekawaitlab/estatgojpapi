f3e66c6e9e8c244661accf88227672ffa200c4d3
osoken <osoken@osoekawalab.com>
2024-06-08 01:11:06 +0900

    test(storage): add test for unknown storage type generation

--- tests/storages/test_factory.py
+++ tests/storages/test_factory.py
@@ -1,6 +1,7 @@
+import pytest
 from pytest_mock import MockerFixture
 
-from estatgojpapi.settings import MemoryStorageSettings
+from estatgojpapi.settings import BaseStorageSettings, MemoryStorageSettings
 from estatgojpapi.storages.factory import create_storage
 
 
@@ -10,3 +11,10 @@ def test_create_storage_creates_memory_storage(mocker: MockerFixture) -> None:
     actual = create_storage(settings=settings)
     assert actual == MemoryStorage.return_value
     MemoryStorage.assert_called_once_with()
+
+
+def test_create_storage_raises_error_when_type_is_unknown(mocker: MockerFixture) -> None:
+    settings = mocker.MagicMock(spec=BaseStorageSettings)
+    settings.type = "UNKNOWN"
+    with pytest.raises(ValueError):
+        create_storage(settings=settings)
